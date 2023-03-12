4439176eaf415e931da55e6e0cc931a9de5da245
prince tanwar <ptanwar468@gmail.com>
2023-03-12 16:34:22 +0530

    add link

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,13 +1,14 @@
 <script>
-
 </script>
-
-
 
 <p>you are viewing products page</p>
 
+<h1><a href="/products/1">product - 1</a></h1>
+<h1><a href="/products/2">product - 2</a></h1>
+<h1><a href="/products/3">product - 3</a></h1>
+
 <style>
-	p{
+	p {
 		color: blue;
 		font-size: 18px;
 		font-weight: bold;
