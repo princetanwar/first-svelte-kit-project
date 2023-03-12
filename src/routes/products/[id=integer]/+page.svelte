657e09edbbf7ddaf29392259ce27fcd838dff799
prince tanwar <ptanwar468@gmail.com>
2023-03-12 11:21:38 +0530

    add route matcher

---
+++ src/routes/products/[id=integer]/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import {page} from '$app/stores';
+	
+</script>
+
+
+<h1>product page for a id {$page.params.id}</h1>
--- src/routes/products/[id]/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	import {page} from '$app/stores';
-	console.log({page: $page.params})
-</script>
-
-
-<h1>product page for a id</h1>
